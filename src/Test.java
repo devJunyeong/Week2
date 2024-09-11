19b006f5f2563afb4258bfe64933e7d8e4c6f336
Junyeon Hwang <202430334@email.daelim.ac.kr>
2024-09-11 17:34:40 +0900

    자바 상수 알아보기

---
+++ src/Test.java
@@ -0,0 +1,12 @@
+public class Test {
+    public static void main(String[] args) {
+        int temperature = 20;
+        final int FREEZING_POINT = 0;
+
+        temperature = 25;
+        System.out.println("Current temperature" + temperature);
+
+        System.out.println("Freezing point of water" + FREEZING_POINT);
+
+    }
+}
